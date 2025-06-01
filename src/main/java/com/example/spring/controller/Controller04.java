c5dd6e9380fedfd87b73c6dea488d5f70acb6153
yumin359 <yumin359@gmail.com>
2025-06-01 23:20:06 +0900

    d

--- src/main/java/com/example/spring/controller/Controller04.java
+++ src/main/java/com/example/spring/controller/Controller04.java
@@ -1,6 +1,7 @@
 package com.example.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -74,5 +75,50 @@ public class Controller04 {
         return "main4/sub4";
     }
 
+    @RequestMapping("sub5")
+    public String sub5(Model model) {
+        // 1.요청 분석/가공
+        // 2.요청 처리(business logic)
 
+        // 3. 결과를 Model에 담기
+        model.addAttribute("name1", "결과 값1");
+        model.addAttribute("name2", "또 다른 결과");
+
+        // 4. view로 forwarding
+        return "main4/sub5";
+    }
+
+
+    // 연습:
+    // /main4/sub6 으로 요청 오면
+    // templates/main4/sub6.html 이 응답 되도록
+    // request handler method 작성
+    @RequestMapping("sub6")
+    public String sub6(Model model) {
+
+        // model attribute : model에 넣은 값(객체)
+        model.addAttribute("name", "손흥민");
+        model.addAttribute("address", "런던");
+        model.addAttribute("email", "야후");
+
+
+        return "main4/sub6";
+    }
+
+    // 연습 : 적절한 html 코드 작성
+    @RequestMapping("sub7")
+    public String sub7(Model model) {
+        model.addAttribute("age", 55);
+        model.addAttribute("score", 97.54);
+        model.addAttribute("school", "서울대");
+        model.addAttribute("city", "신림동");
+
+        return "main4/sub7";
+    }
+
+    @RequestMapping("sub8")
+    public String sub8(Model model) {
+        model.addAttribute("number", 1234567);
+        return "main4/sub8";
+    }
 }
