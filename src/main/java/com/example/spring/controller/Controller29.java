674a44363171d8d5a05015cbc20e493cac58ff67
yumin359 <yumin359@gmail.com>
2025-07-07 12:31:46 +0900

    0707 json post 요청 받기

--- src/main/java/com/example/spring/controller/Controller29.java
+++ src/main/java/com/example/spring/controller/Controller29.java
@@ -2,8 +2,11 @@ package com.example.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/api/main29")
@@ -26,6 +29,21 @@ public class Controller29 {
     @PostMapping("sub3")
     public String sub3() {
         System.out.println("Controller29.sub3");
+        return null;
+    }
+
+    @PostMapping("sub4")
+    public String sub4(@RequestBody Map<String, Object> map) {
+        // @RequestBody : JSON을 해당 타입(Map)으로 역직렬화(디코딩, 파싱) 해주는 어노테이션
+        System.out.println("Controller29.sub4");
+        System.out.println("map = " + map);
+        return null;
+    }
+
+    @PostMapping("sub5")
+    public String sub5(@RequestBody Map<String, Object> map) {
+        System.out.println("Controller29.sub5");
+        System.out.println("map = " + map); // {address=seoul, score=88.12, country=korea}
         return null;
     }
 
