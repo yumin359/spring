e8deb7f0b747e6107bb3ab86231a6a2811a01bc9
yumin359 <yumin359@gmail.com>
2025-07-08 10:53:42 +0900

    0708 map 객체 json으로 직렬화 해서 응답

--- src/main/java/com/example/spring/controller/Controller32.java
+++ src/main/java/com/example/spring/controller/Controller32.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping("/api/main32")
 public class Controller32 {
@@ -23,5 +25,20 @@ public class Controller32 {
     @ResponseBody
     public String sub2() {
         return "응답 데이터";
+    }
+
+    @GetMapping("sub3")
+    @ResponseBody
+    public Map<String, Object> sub3() {
+        // 객체(dto, bean)나 map으로 응답 가능
+        // -> JSON으로 직렬화 됨
+        return Map.of("name", "흥민", "age", 88);
+    }
+
+    // 연습 : react에 4번째 버튼 클릭시, "서울, 신촌, 한국" 출력되도록 코드 작성
+    @GetMapping("sub4")
+    @ResponseBody
+    public Map<String, Object> sub4() {
+        return Map.of("city", "서울", "address", "신촌", "country", "한국");
     }
 }
